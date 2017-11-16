006eb13013fd25aa0e67ebe67593895689c22f3c
Zohar <zoharavr@post.bgu.ac.il>
2017-11-16 13:09:23 +0200

    try 5000001

---
+++ src/Parse.java
@@ -0,0 +1,24 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.regex.Pattern;
+
+public class Parse {
+    private LinkedList <String> text;
+
+   public Parse (LinkedList<String> s){
+       this.text=s;
+   }
+public void ParseNumbers (){
+    Iterator<String> itr=text.iterator();
+    while (itr.hasNext()){
+       String s= itr.next();
+       String [] need_to_parse=s.split("( )|(  )");
+       for(int i=0;i<need_to_parse.length;i++ ){
+           if(need_to_parse[i].matches("\\d+\\.\\d+")){
+
+           }
+       }
+    }
+}
+}
+
