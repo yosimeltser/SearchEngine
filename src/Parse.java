512a90b6e477acf3bbb732e46e72be77e1b450a9
Zohar <zoharavr@post.bgu.ac.il>
2017-11-25 20:20:15 +0200

    after yosi made a mess

--- src/Parse.java
+++ src/Parse.java
@@ -1,45 +1,55 @@
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.regex.Pattern;
 
 
 public class Parse {
     private LinkedList <String> text;
+    private static Pattern pattern = Pattern.compile("\\s+");
 
-
-   public Parse (LinkedList<String> s){
-       this.text=s;
-   }
-public void ParseNumbers (){
-    Iterator<String> itr=text.iterator();
-    while (itr.hasNext()){
-       String s= itr.next();
-       int x;
-       String [] need_to_parse=s.split("\\s+");
-       for(int i=0;i<need_to_parse.length;i++ ){
-           need_to_parse[i]=delCommas(need_to_parse[i]);
-           need_to_parse[i]=roudUp(need_to_parse[i]);
-           need_to_parse[i]=convPrecent(need_to_parse[i]);
-           if ((x = checkIfMonth(need_to_parse[i])) > 0) {
-               if (i < need_to_parse.length) {
-                   i=month_year(x,need_to_parse,i);
-               }
-           }
+    public Parse (LinkedList<String> s){
+        this.text=s;
     }
+    public void ParseNumbers (){
+        Iterator<String> itr=text.iterator();
+        while (itr.hasNext()){
+            String s= itr.next();
+            int x;
+            String [] need_to_parse=pattern.split(s);
+            for(int i=0;i<need_to_parse.length;i++ ){
+                need_to_parse[i]=delCommas(need_to_parse[i]);
+                need_to_parse[i]=roudUp(need_to_parse[i]);
+                need_to_parse[i]=convPrecent(need_to_parse[i]);
+                USA(need_to_parse,i);
+                if ((x = checkIfMonth(need_to_parse[i])) > 0) {
+                    if (i < need_to_parse.length) {
+                        i=month_year(x,need_to_parse,i);
+                    }
+                }
+            }
+        }
     }
-}
-
-private static int checkIfMonth(String s){
-     String[]month={"January","February","March","April","May","June","July","August","September","October","November","December"};
-    for (int i = 0; i <month.length ; i++) {
-        if(month[i].equalsIgnoreCase(s))
-            return i+1;
+    //NEW RULE
+//We have noticed that the token U.S has a lot of instances
+//So we decided that there is more chances that the user serached for "usa" instead U.A
+    private static void USA (String [] need_to_parse,int i) {
+        if (need_to_parse[i].equals("U.S")) {
+            need_to_parse[i]= "usa";
+        }
     }
-    return  -1;
-}
+    //returns the number of a month if  string s is a month
+    private static int checkIfMonth(String s){
+        String[]month={"January","February","March","April","May","June","July","August","September","October","November","December"};
+        for (int i = 0; i <month.length ; i++) {
+            if(month[i].equalsIgnoreCase(s))
+                return i+1;
+        }
+        return  -1;
+    }
     //Example: from 3.55555 -> 3.56
     //converts the second digit after the floating point up
     private  String roudUp(String s) {
-        if(s.matches("\\d+\\.\\d+")) {
+        if(Pattern.compile("\\d+\\.\\d+").matcher(s).matches()) {
             double x=Double.parseDouble(s);
             x = Math.round(x*100);
             x=x/100 ;
@@ -51,7 +61,7 @@ private static int checkIfMonth(String s){
     //Example: 1,345 -> 1345
     //removes commas from the numbers
     private  String delCommas(String s) {
-        if(s.matches("^(([-+] ?)?[0-9]+(,[0-9]+)+)?((.[0-9]+))")) {
+        if(Pattern.compile("^(([-+] ?)?[0-9]+(,[0-9]+)+)?((.[0-9]+))").matcher(s).matches()) {
             s.contains(",");
             return  s.replace(",","");
         }
@@ -59,8 +69,7 @@ private static int checkIfMonth(String s){
     }
 
     private  String convPrecent (String s){
-        String n= s.replaceAll("%|perecentge"," percent");
-        return n;
+        return Pattern.compile("%|perecentge").matcher(s).replaceAll(" percent");
     }
     private int month_year(int x, String[] arr, int i){
         String s="";
@@ -70,22 +79,23 @@ private static int checkIfMonth(String s){
         else {
             s = x+"";
         }
+
         //fits to the next pattens
         //DDth Month YYYY, DD Month YYYY , DD Month YY , Month Year
-        if(arr[i+1].matches("^\\d{4}$|\\d{2}$")){
-            if(i>0 && arr[i-1].matches("^\\d{1,2}[th]+$"))
+        if(Pattern.compile("^\\d{4}$|\\d{2}$").matcher(arr[i+1]).matches()){
+            if(i>0 && Pattern.compile("^\\d{1,2}[th]+$").matcher(arr[i-1]).matches())
             {
                 arr[i-1]=formattingDayMonth(arr[i-1]);
                 s=arr[i-1].substring(0,arr[i-1].length()-2)+"/"+s+"/"+arr[i+1]  ;
                 arr[i-1]=s;
                 arr[i]="";
                 arr[i+1]="";
-                return i+2;
+                return i+1;
             }
-            else if(i>0 && arr[i-1].matches("^\\d{1,2}$"))
+            else if(i>0 && Pattern.compile("^\\d{1,2}$").matcher(arr[i-1]).matches())
             {
                 arr[i-1]=formattingDayMonth(arr[i-1]);
-                if (arr[i+1].matches("^\\d{2}$")){
+                if ( Pattern.compile("^\\d{2}$").matcher(arr[i+1]).matches()){
                     s = arr[i - 1] + "/" + s + "/" +"19"+ arr[i + 1];
                 }
                 else  {
@@ -94,19 +104,19 @@ private static int checkIfMonth(String s){
                 arr[i-1]=s;
                 arr[i]="";
                 arr[i+1]="";
-                return i+2;
+                return i+1;
 
             }
-            else if (arr[i+1].matches("^\\d{4}$")){
+            else if ( Pattern.compile("^\\d{4}$").matcher(arr[i+1]).matches()){
                 s=s+"/"+arr[i+1];
                 arr[i]=s;
                 arr[i+1]="";
+                return i+1;
             }
         }
         //Month DD , Month DD YYYY
-         if (arr[i+1].matches("^\\d{1,2}[,]*")){
-            arr[i+1]=formattingDayMonth(arr[i+1]);
-            if (arr[i+2].matches("^\\d{4}$")){
+        if (Pattern.compile("^\\d{1,2}[,]*").matcher(arr[i+1]).matches()){
+            if ( Pattern.compile("^\\d{4}$").matcher(arr[i+2]).matches()){
                 s = arr[i+1].substring(0,arr[i+1].length()-1) + "/" + s + "/" + arr[i + 2];
                 arr[i]=s;
                 arr[i+1]="";
@@ -120,23 +130,30 @@ private static int checkIfMonth(String s){
                 return i+1;
             }
         }
-        if (i>0 && arr[i-1].matches("^\\d{1,2}$")) {
+        if (i>0 && Pattern.compile("^\\d{1,2}$").matcher(arr[i-1]).matches()) {
             arr[i-1]=formattingDayMonth(arr[i-1]);
             s = arr[i-1] + "/" + s;
             arr[i-1]=s;
             arr[i]="";
             return i;
         }
-     return 0;
+        //NEW RULE
+        //first rule
+        //22-23 January => 22/01 23/01
+        if (Pattern.compile("^\\d{2}-\\d{2}$").matcher(arr[i-1]).matches()){
+            arr[i-1]=arr[i-1].substring(0,2)+"/"+s;
+            arr[i]=arr[i-1].substring(3,5)+"/"+s;
+            return i;
+        }
+        return i;
     }
     private String formattingDayMonth(String s) {
-        if (s.matches("^\\d([th ,])*$")){
+        if (Pattern.compile("^\\d([th ,])*$").matcher(s).matches()){
             return "0"+s;
         }
         else {
             return s;
         }
     }
-
 }
 
