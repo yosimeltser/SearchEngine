2a5e44265107c6139a933dc597addb9955319231
Zohar <zoharavr@post.bgu.ac.il>
2017-12-15 14:33:20 +0200

    add a function delete dots. need to check it as quick as we can

--- src/View/StartController.java
+++ src/View/StartController.java
@@ -1,6 +1,7 @@
 package View;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
@@ -12,36 +13,46 @@ public class StartController {
     Stage ds;
     @FXML
     public TextField txt_corpus, txt_posting;
+    public CheckBox check_stem;
 
-    public void choose_corpus() {
+    public void set(Stage sc) {
+        this.ds = sc;
+    }
+    //tells is the user choose
+    public boolean getStemOrNot() {
+        return check_stem.isSelected();
+    }
+//NOTICE that the corpus & the file of the stopwords need to be at the same directory!
+    public String choose_corpus() {
         DirectoryChooser directoryChooser = new DirectoryChooser();
         File selectedDirectory =
                 directoryChooser.showDialog(ds);
 
         if (selectedDirectory == null) {
             txt_corpus.setText("No Directory selected");
+            return  null;
         } else {
             txt_corpus.setText(selectedDirectory.getAbsolutePath());
+            return selectedDirectory.getAbsolutePath();
         }
-    }
-
-    public void set(Stage sc) {
-        this.ds = sc;
     }
 
     public void close_win() {
         ds.close();
     }
-
-    public void choose_to_save() {
+// gives u the path where to save the the posting & dictionary.
+    //need to check that the return argument isn't null
+    public String choose_to_save() {
         DirectoryChooser directoryChooser = new DirectoryChooser();
         File selectedDirectory =
                 directoryChooser.showDialog(ds);
 
         if (selectedDirectory == null) {
             txt_posting.setText("No Directory selected");
+            return null;
         } else {
             txt_posting.setText(selectedDirectory.getAbsolutePath());
+            return selectedDirectory.getAbsolutePath();
         }
     }
 
