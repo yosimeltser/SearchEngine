26efa2856706c2be95d65cded3d1828095555593
yosimeltser <yosefmel@post.bgu.ac.il>
2017-12-31 22:09:45 +0200

    CHANGES IN EXPAND REMEMBER TO CHANGE THE POWER

--- src/Model/ExpandQuery.java
+++ src/Model/ExpandQuery.java
@@ -4,41 +4,42 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.util.Dictionary;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class ExpandQuery {
+    String query;
+    HashMap<String, Double> term_weight;
 
-    public  void expand(){
+    public ExpandQuery(String s) {
+        this.query = s;
+        term_weight = new HashMap<>();
+    }
+
+    public void expand() {
         try {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Type something that you want me to search on the internet...");
-            String nextLine = scanner.nextLine();
-            if (nextLine == null)
-                System.out.println("enter query");
-            System.out.println("Searching on the web....");
-            String wikipediaApiJSON = "https://en.wikipedia.org/wiki/"
-                    + nextLine;
+            String wikipediaApiJSON = "https://en.wikipedia.org/wiki/" + query;
             //connect to wikipedia with the one word query
             org.jsoup.nodes.Document doc = Jsoup.connect(wikipediaApiJSON).ignoreContentType(true).get();
             // selects all the body content
             Elements data = doc.select("#bodyContent");
             //selects all the paragraphs
             Elements paragraphs = data.select("p");
-            //checks if we have if the query Can be interpreted to more than one term
+            //checks if we have the query Can be interpreted to more than one term
             if (paragraphs.first().text().contains(" may refer to:")) {
                 multiple_values(data);
             } else {
                 //iterates the paragraphs and takes words that are in bold or anchor
                 for (Element p : paragraphs) {
-                    System.out.println(p.getElementsByTag("b").text());
-                    for (Element e : p.getElementsByTag("a")) {
-                        Element divGuarantee = e.parent();
-                        if (divGuarantee.is("sup"))
-                            continue;
-                        else {
-                            System.out.println(e.text());
-                        }
-                    }
+                    if (term_weight.size() < 6) {
+                        Elements bolds = p.getElementsByTag("b");
+                        //check if we got any
+                        if (bolds.size() > 0)
+                            insert_bolds(bolds);
+                        insert_anchors( p.getElementsByTag("a"));
+
+                    } else break;
                 }
             }
 
@@ -47,16 +48,45 @@ public class ExpandQuery {
         }
     }
 
-    public static void multiple_values(Elements data) {
+    private void insert_bolds(Elements bs) {
+        for (Element b : bs) {
+            if (term_weight.size() < 6) {
+                String word = b.text();
+                word = word.toLowerCase();
+                //checks that the word is not equal to the query
+                if (!word.equals(query)) {
+                    term_weight.put(word, 1.0);
+                }
+            } else break;
+        }
+    }
+
+    private void insert_anchors(Elements as) {
+        for (Element e :as) {
+            if (term_weight.size() < 6) {
+                Element divGuarantee = e.parent();
+                if (divGuarantee.is("sup"))
+                    continue;
+                else {
+                    //REMEMBER TO CHANGE
+                   term_weight.put(e.text(),0.7);
+                }
+            }
+            else break;
+        }
+    }
+
+    private void multiple_values(Elements data) {
         Elements li = data.select("ul").not("h2");
         Elements as = li.select("a");
-
         for (Element e : as) {
-            Elements f= e.select("span");
-            if(f.size()>0)
-                continue;
-            System.out.println(e.attr("title"));
-
+            if (term_weight.size() < 6) {
+                Elements f = e.select("span");
+                if (f.size() > 0)
+                    continue;
+                System.out.println(e.attr("title"));
+            } else break;
         }
+
     }
 }
