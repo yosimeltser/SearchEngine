307cc804a9328203b56832dabd58309b0c7fdd62
yosefmel <yosefmel@post.bgu.ac.il>
2018-01-03 16:59:16 +0200

    ranker with sort

--- src/Model/Ranker.java
+++ src/Model/Ranker.java
@@ -17,16 +17,21 @@ public class Ranker {
     //dictionary loaded from the Load Class
     HashMap<String,String> dictionary;
     //maps from doc name to query rank...
-    HashMap<String,Double> docToRank;
+    HashMap <String,Double> docToRank;
+    //sorted docToRank
+    LinkedHashMap<String,Double> docToRamkSorted;
     //all the words returned from the query after parse and stem
     ArrayList <String> query;
     //cache loaded from the Load Class
     ArrayList<String> cache;
+
     public void Cosin(){
         //just for getting fields from the load class
         Load l =new Load();
         dictionary=l.getDictionary();
         cache=l.getLoadedCache();
+        docToRank=  new HashMap<String,Double>();
+        setQuerySize(query.size());
         for (String term:query) {
             //get rank for each term
             //put the value to the dictionary
@@ -67,23 +72,28 @@ public class Ranker {
         //if the word is in the cache
         else {
             int line= Integer.parseInt(s.substring(j+1,s.indexOf('D')));
-            postLine=cache.get(line).split("[<>, ]");
+            postLine=cache.get(line).split("[<>,//s+]");
         }
         //starts from 1 hence the term in the 0 place
         for (int i=1;i<postLine.length;i++){
-            String docNumber=postLine[i]; 
-            long tf= Long.parseLong(postLine[++i]);
-            long index = Long.parseLong(postLine[++i]);
-            weight (getTermDF(term),docNumber,tf,index);
+            if (!postLine[i].equals(" ")) {
+                String docNumber=postLine[i];
+                long tf= Long.parseLong(postLine[++i]);
+                long index = Long.parseLong(postLine[++i]);
+                weight (getTermDF(term),docNumber,tf,index);
+            }
         }
+        docToRamkSorted=sortByValue(docToRank);;
     }
 
+
+
     private void weight(long df,String docNumber, long tf, long index) {
-        long docRank;
-        long maxDocTf;
-        long docSize;
+        double docRank;
+        double maxDocTf;
+        double docSize;
         double oneWordQuery;
-        try (BufferedReader br = new BufferedReader(new FileReader("docs_weights_not_stemmed.txt"))) {
+        try (BufferedReader br = new BufferedReader(new FileReader("docs_weights_NoStem.txt"))) {
             String line;
             while ((line = br.readLine()) != null) {
                 int space=line.indexOf("*");
@@ -91,9 +101,9 @@ public class Ranker {
                 //if the document found break
                 if (docN.equals(docNumber)) {
                     String [] docProp= line.split("\\*");
-                    docRank = Long.parseLong(docProp[1]);
-                    maxDocTf = Long.parseLong(docProp[2]);
-                    docSize = Long.parseLong(docProp[3]);
+                    docRank = Double.parseDouble(docProp[1]);
+                    maxDocTf = Double.parseDouble(docProp[2]);
+                    docSize = Double.parseDouble(docProp[3]);
                     oneWordQuery=(tf/maxDocTf)*((docSize-index)/docSize)*(Math.log10((467767/df)));
                     double cossin=oneWordQuery/((Math.sqrt(querySize))* docRank);
                     if (docToRank.containsKey(docNumber)) {
@@ -119,5 +129,22 @@ public class Ranker {
             e.printStackTrace();
         }
         return s;
+    }
+    //code from
+    private   <K, V> LinkedHashMap<K, V> sortByValue(HashMap<K, V> map) {
+        List<Map.Entry<K, V>> list = new LinkedList<>(map.entrySet());
+        Collections.sort(list, new Comparator<Object>() {
+            @SuppressWarnings("unchecked")
+            public int compare(Object o1, Object o2) {
+                return ((Comparable<V>) ((Map.Entry<K, V>) (o2)).getValue()).compareTo(((Map.Entry<K, V>) (o1)).getValue());
+            }
+        });
+
+        LinkedHashMap<K, V> result = new LinkedHashMap<>();
+        for (Iterator<Map.Entry<K, V>> it = list.iterator(); it.hasNext();) {
+            Map.Entry<K, V> entry = (Map.Entry<K, V>) it.next();
+            result.put(entry.getKey(), entry.getValue());
+        }
+        return result;
     }
 }
